5bfb7731bb23bc14d26c5fab045d4c7c269425c0
Logan Hendry <l.hendry@shaw.ca>
2024-12-21 16:39:19 -0800

    Initial Commit

---
+++ Scripts/escape_menu.gd
@@ -0,0 +1,43 @@
+extends CanvasLayer
+
+#tracks whether the menu is visable
+var menuVisible = false
+
+@onready var menuPanel = $Panel
+@onready var resumeButton = menuPanel.get_node("VBoxContainer/ResumeButton")
+@onready var settingsButton = menuPanel.get_node("VBoxContainer/SettingsButton")
+@onready var quitButton = menuPanel.get_node("VBoxContainer/QuitButton")
+
+func _ready():
+	#initially hide the menu
+	menuPanel.visible = false
+
+	#connect the buttons
+	resumeButton.connect("pressed", Callable(self, "_on_resume_button_pressed"))
+	settingsButton.connect("pressed", Callable(self, "_on_settings_button_pressed"))
+	quitButton.connect("pressed", Callable(self, "_on_quit_button_pressed"))
+
+
+func _input(event):
+	#toggle the menu when escape is pressed
+	if event.is_action_pressed("ui_cancel"):
+		toggle_menu()
+		
+
+func toggle_menu():
+	menuVisible = !menuVisible
+	menuPanel.visible = menuVisible
+	get_tree().paused = menuVisible #pause game when menu visible
+	
+#button actions
+#close menu
+func _on_resume_button_pressed():
+	toggle_menu()
+	
+#open settings menu
+func _on_settings_button_pressed():
+	print("Open Settings Menu")
+	
+#quit game
+func _on_quit_button_pressed():
+	get_tree().quit()
---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var speed = 150
+@export var gravity = 30
+@export var jumpForce = 500
+#https://www.youtube.com/watch?v=LOhfqjmasi0&ab_channel=Brackeys
+
+@onready var animatedSprite = $AnimatedSprite2D
+
+func _physics_process(delta: float):
+	#adds Gravity
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+	
+	#Handle Jump
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jumpForce
+	
+	# get the input direction of the player
+	var horizontalDirection = Input.get_axis("move_left", "move_right")
+	
+	#flip the sprite
+	if horizontalDirection > 0:
+		animatedSprite.flip_h = false
+	elif horizontalDirection < 0:
+		animatedSprite.flip_h = true
+		
+	#play animations
+	if is_on_floor():
+		if horizontalDirection == 0:
+			animatedSprite.play("Player_idle")
+		else:
+			animatedSprite.play("Player_walk")
+	else:
+		animatedSprite.play("Player_jump")
+	
+	#Handle Movement
+	velocity.x = horizontalDirection * speed
+	
+	move_and_slide()
