3428c697c85decaa99cfd42ce80f7593b3dcf6b8
kaleblieuweblahut <kaleblieuweblahut@gmail.com>
2024-12-21 20:15:16 -0800

    objects deaded

--- killzone.gd
+++ killzone.gd
@@ -2,9 +2,14 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body: Node2D) -> void:
-	print("Flirted With Death")
-	timer.start()
+func _on_body_entered(body:Node2D ) -> void:
+	if body == $"../Player":
+		print("Flirted With Death")
+		timer.start()
+	
+	if body != $"../Player":
+		body.queue_free()
+		print("object killed")
 
 
 func _on_timer_timeout() -> void:
